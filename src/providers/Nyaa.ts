ad0c34f4a7724e25bedb060a2a07f15d3529d15c
SamTheRuby <53793412+SamTheRuby@users.noreply.github.com>
2024-05-14 11:23:15 -0400

    Update Nyaa.ts to format regex

--- src/providers/Nyaa.ts
+++ src/providers/Nyaa.ts
@@ -1,47 +1,48 @@
-import { nyaaUrl } from '#/constants'
+import { nyaaUrl } from '#/constants';
 import {
   INyaaData,
   IProvider,
   ITorrentRelease,
   ISneedexRelease
-} from '#interfaces/index'
-import { app } from '#/index'
-import { Utils } from '#utils/Utils'
-import { load } from 'cheerio'
+} from '#interfaces/index';
+import { app } from '#/index';
+import { Utils } from '#utils/Utils';
+import { load } from 'cheerio';
 
 export class Nyaa implements IProvider {
-  readonly name: string
+  readonly name: string;
   constructor() {
-    this.name = 'Nyaa'
+    this.name = 'Nyaa';
   }
 
-  // provider specific fetch function to retrieve raw data
   private async fetch(query: string): Promise<INyaaData> {
-    Utils.debugLog(this.name, 'cache', `${this.name}_${query}`)
-    const cachedData = await app.cache.get(`${this.name}_${query}`)
+    Utils.debugLog(this.name, 'cache', `${this.name}_${query}`);
+    const cachedData = await app.cache.get(`${this.name}_${query}`);
     if (cachedData) {
-      Utils.debugLog(this.name, 'cache', `Cache hit: ${this.name}_${query}`)
-      return cachedData as INyaaData
+      Utils.debugLog(this.name, 'cache', `Cache hit: ${this.name}_${query}`);
+      return cachedData as INyaaData;
     }
-    Utils.debugLog(this.name, 'cache', `Cache miss: ${this.name}_${query}`)
+    Utils.debugLog(this.name, 'cache', `Cache miss: ${this.name}_${query}`);
 
-    const scrapeUrl = `https://nyaa.si/view/${query}`
-    Utils.debugLog(this.name, 'fetch', query)
-    Utils.debugLog(this.name, 'fetch', `Fetching data from ${scrapeUrl}`)
-    // Scrape the HTML for the info
+    const scrapeUrl = `https://nyaa.si/view/${query}`;
+    Utils.debugLog(this.name, 'fetch', query);
+    Utils.debugLog(this.name, 'fetch', `Fetching data from ${scrapeUrl}`);
+
     const html = await fetch(scrapeUrl).then(res => {
-      if (!res.ok) throw new Error(res.statusText)
-      return res.text()
-    })
+      if (!res.ok) throw new Error(res.statusText);
+      return res.text();
+    });
 
-    const $ = load(html)
+    const $ = load(html);
 
     const scrapedData = {
       title: $('body > div > div:nth-child(1) > div.panel-heading > h3')
         .text()
         .trim(),
       date: $('div.row:nth-child(1) > div:nth-child(4)').text().trim(),
-      seeders: +$('div.row:nth-child(2) > div:nth-child(4) > span:nth-child(1)')
+      seeders: +$(
+        'div.row:nth-child(2) > div:nth-child(4) > span:nth-child(1)'
+      )
         .text()
         .trim(),
       leechers: +$(
@@ -51,97 +52,92 @@ export class Nyaa implements IProvider {
         .trim(),
       size: $('div.row:nth-child(4) > div:nth-child(2)').text().trim(),
       completed: +$('div.row:nth-child(4) > div:nth-child(4)').text().trim(),
-      infohash: $('div.row:nth-child(5) > div:nth-child(2) > kbd:nth-child(1)')
+      infohash: $(
+        'div.row:nth-child(5) > div:nth-child(2) > kbd:nth-child(1)'
+      )
         .text()
         .trim(),
       files: $(
         '.torrent-file-list > ul:nth-child(1) > li:nth-child(1) > ul:nth-child(2)'
       ).find('li').length,
       id: +query
-    }
+    };
 
     Utils.debugLog(
       this.name,
       'fetch',
       `Fetched data, caching ${this.name}_${query}`
-    )
-    await app.cache.set(`${this.name}_${query}`, scrapedData)
+    );
+    await app.cache.set(`${this.name}_${query}`, scrapedData);
 
-    return scrapedData as INyaaData
+    return scrapedData as INyaaData;
+  }
+
+  private formatTitle(originalTitle: string): string {
+    const releaseGroupRegex = /^\[([^\]]+)]/;
+    const releaseGroupMatch = originalTitle.match(releaseGroupRegex);
+    const releaseGroup = releaseGroupMatch ? releaseGroupMatch[1] : '';
+
+    const showNameRegex = /\]\s*([^[(\]\[]+[^)\]\[])\s*(?:(?<!Season|[Ss]\d+|Arc)\(|\[|\])/;
+    const seasonRegex = /(?:Season|S|Arc)\s*(\d+|Arc)/i;
+    const resolutionRegex = /\b(?<!264|265)(?:(\d{3,4}x\d{3,4})|([1-9]\d{2,3}p))\b/;
+    const sourceRegex = /\b(?:BD(?:-?rip)?|BluRay|WEB(?:-?rip)?|HDTV(?:-?WEB)?|DVD(?:-?rip)?|JPBD|USBD|ITABD|R1\s?DVD|R2\s?DVD|R2J|R1J)\b/i;
+    const versionRegex = /\bv[0-4]\b/i;
+    const szenjdReleaseGroupRegex = /SZNJD-([^\s]+)/i;
+
+    let formattedTitle = `${originalTitle.match(showNameRegex)?.[1] || ''} ${originalTitle.match(seasonRegex)?.[0] || ''} ${originalTitle.match(resolutionRegex)?.[0] || ''} ${originalTitle.match(sourceRegex)?.[0] || ''} ${originalTitle.match(versionRegex)?.[0] || ''}`;
+
+    const szenjdReleaseGroupMatch = originalTitle.match(szenjdReleaseGroupRegex);
+    const szenjdReleaseGroup = szenjdReleaseGroupMatch ? szenjdReleaseGroupMatch[1] : '';
+
+    formattedTitle = `${formattedTitle} SZNJD-${szenjdReleaseGroup}`;
+
+    return formattedTitle.trim();
   }
 
   public async get(
     anime: { title: string; alias: string },
     sneedexData: ISneedexRelease
   ): Promise<ITorrentRelease[]> {
-    /*
-    // try to find the torrent using Nyaa's RSS API
-    // this used fast-xml-parser
-    const nyaaRSS = `https://nyaa.si/?page=rss&cats=1_2&term=${encodeURIComponent(
-      anime.title
-    )}`
-
-    const nyaaData = await fetch(nyaaRSS).then(res => {
-      if (!res.ok) throw new Error(res.statusText)
-      return res.text()
-    })
-
-    // parse the RSS feed to return an array of torrents
-    const {
-      rss: {
-        channel: { item }
-      }
-    } = new XMLParser().parse(nyaaData)
-
-    // search the items in the array's download links for the nyaaID
-    // the property is item.guid
-    const matchedNyaa = item.find(
-      (item: any) => +item.guid.match(/nyaa.si\/view\/(\d+)/)[1] === nyaaID
-    )
-    */
-
     const bestReleaseLinks = sneedexData.best_links.length
       ? sneedexData.best_links.split(' ')
-      : sneedexData.alt_links.split(' ')
+      : sneedexData.alt_links.split(' ');
 
-    // get each Nyaa link and scrape the page
     const nyaaLinks = bestReleaseLinks.filter((url: string) =>
       url.includes('nyaa.si/view/')
-    )
+    );
     const nyaaIDs = nyaaLinks.length
       ? nyaaLinks.map((url: string) => +url.match(/nyaa.si\/view\/(\d+)/)[1])
-      : null
+      : null;
 
-    // for each nyaaID, scrape the page and return the data
     const nyaaData = nyaaIDs
       ? await Promise.all(
           nyaaIDs.map(async (nyaaID: number) => {
-            const nyaaData = await this.fetch(`${nyaaID}`)
-            return nyaaData
+            const nyaaData = await this.fetch(`${nyaaID}`);
+            return nyaaData;
           })
         )
-      : null
+      : null;
 
-    // if there's no nyaa data, return null
-    if (!nyaaData) return null
+    if (!nyaaData) return null;
 
-    // format the data into an array of ITorrentReleases
     const releases: ITorrentRelease[] = nyaaData.map((data: INyaaData) => {
-      const size = data.size.split(' ')
+      const formattedTitle = this.formatTitle(data.title);
+
+      const size = data.size.split(' ');
       const sizeInBytes =
         size[1] === 'GiB'
           ? +size[0] * 1024 * 1024 * 1024
-          : +size[0] * 1024 * 1024
+          : +size[0] * 1024 * 1024;
 
-      // remove decimal places from the size in bytes to comply with Torznab
-      const sizeInBytesRounded = Math.round(sizeInBytes)
+      const sizeInBytesRounded = Math.round(sizeInBytes);
 
       const formattedDate = Utils.formatDate(
         new Date(data.date.replace(' UTC', '')).getTime()
-      )
+      );
 
       return {
-        title: data.title,
+        title: formattedTitle,
         link: `https://nyaa.si/view/${data.id}`,
         url: `https://nyaa.si/download/${data.id}.torrent`,
         seeders: data.seeders,
@@ -152,9 +148,9 @@ export class Nyaa implements IProvider {
         timestamp: formattedDate,
         grabs: data.completed,
         type: 'torrent'
-      }
-    })
+      };
+    });
 
-    return releases as ITorrentRelease[]
+    return releases as ITorrentRelease[];
   }
 }
